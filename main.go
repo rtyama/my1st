dc6ad92ae4d8adc63e728fbc8f128bf4d9e9161e
rtyama <142071441+rtyama@users.noreply.github.com>
2023-08-12 13:05:09 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println(1 + 1 + 1)
+	for {
+		ret := rand.Intn(100)
+		if hoge(ret) {
+			fmt.Println("My favorite number is", ret)
+			break
+		}
+		fmt.Println("失敗またやります")
+	}
+}
+
+func hoge(r int) bool {
+	if r > 50 {
+		return true
+	}
+	return false
 }
